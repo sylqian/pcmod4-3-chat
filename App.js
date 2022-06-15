c09b77f695409781030fe7986b7b86f75474ecdd
sylvie <sylvieqian@hotmail.com>
2022-06-15 12:36:44 +0800

    add stack screen

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import ChatScreen from "./screens/ChatScreen";
+import LoginScreen from "./screens/LoginScreen";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+<NavigationContainer>
+  <Stack.Navigator> 
+    <Stack.Screen name="Login" component={LoginScreen} />
+    <Stack.Screen name="Chat" component={ChatScreen} />
+  </Stack.Navigator>
+</NavigationContainer>
   );
 }
 
