8ba39f4f0539591c1a637e302573ebe6e28ad8a4
sylvie <sylvieqian@hotmail.com>
2022-06-15 12:25:44 +0800

    add chat and login screen

---
+++ screens/ChatScreen.js
@@ -0,0 +1,13 @@
+// rnfs
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function ChatScreen() {
+  return (
+    <View>
+      <Text>ChatScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
