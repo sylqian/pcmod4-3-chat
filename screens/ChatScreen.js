9a344fc620c534d5adcef5a2ad01480748beb436
sylvie <sylvieqian@hotmail.com>
2022-06-16 12:01:25 +0800

    add gifted chat

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,8 +1,25 @@
 // rnfs
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { GiftedChat } from 'react-native-gifted-chat'
+
+const demoMessage =	{
+    _id: 1,
+    text: 'Hello there!',
+    createdAt: new Date(),
+    user: {
+        _id: 2,
+        name: 'Demo person',
+        avatar: 'https://placeimg.com/140/140/any'
+    }
+}
 
 export default function ChatScreen() {
+    useState [messages, setMessages] = useState([])
+    useEffect (() => {
+        setMessages([demoMessage])
+    }, [])
+
   return (
     <View>
       <Text>ChatScreen</Text>
